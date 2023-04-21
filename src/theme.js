ba53341b8bbc2fe1ca9696bfd93b5a2d93690a1d
crohnaro <viniciosalfa@hotmail.com>
2023-04-21 19:30:16 -0300

    style: :lipstick: defining the main theme of the app

---
+++ src/theme.js
@@ -0,0 +1,20 @@
+import { createTheme } from "@mui/material";
+
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#A835FF',
+        },
+        secondary: {
+            main: "#A187B0"
+        },
+        background: {
+            default: "#3B2A45",
+            white: "#FFF6FF"
+        }
+    },
+    
+})
+
+export default theme
