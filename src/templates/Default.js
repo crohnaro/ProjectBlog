36b35a9b119c74bd3f5a1b6e6aa4ea086eecc99d
crohnaro <crohnarodev@gmail.com>
2023-04-22 23:06:52 -0300

    feat: :sparkles: adding template default to the project

---
+++ src/templates/Default.js
@@ -0,0 +1,14 @@
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
+
+const Default = ({children}) => {
+    return (
+        <>
+            <Header />
+                {children}
+            <Footer />
+        </>
+    )
+}
+
+export default Default
