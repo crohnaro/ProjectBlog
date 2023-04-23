bfee3262013f6fa7d0455daa1eec22d149cbe38a
crohnaro <crohnarodev@gmail.com>
2023-04-22 23:10:01 -0300

    style: :lipstick: changing colors

--- src/theme.js
+++ src/theme.js
@@ -1,20 +1,18 @@
 import { createTheme } from "@mui/material";
 
-
 const theme = createTheme({
-    palette: {
-        primary: {
-            main: '#A835FF',
-        },
-        secondary: {
-            main: "#A187B0"
-        },
-        background: {
-            default: "#3B2A45",
-            white: "#FFF6FF"
-        }
+  palette: {
+    primary: {
+      main: "#bc81e9",
     },
-    
-})
+    secondary: {
+      main: "#FFF6FF",
+    },
+    background: {
+      default: "#201925",
+      white: "#FFF6FF",
+    },
+  },
+});
 
-export default theme
+export default theme;
