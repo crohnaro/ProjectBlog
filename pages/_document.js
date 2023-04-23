650519981eabe4fdfff6821c0fd9ed5efb6b90b3
crohnaro <crohnarodev@gmail.com>
2023-04-22 23:12:46 -0300

    style: :lipstick: adding the meta tags to use the theme on the project

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+import theme from '../src/theme'
+
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        {/* PWA primary color */}
+        <meta name="theme-color" content={theme.palette.background.default} />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="emotion-insertion-point" content="" />
+      </Head>
       <body>
         <Main />
         <NextScript />
