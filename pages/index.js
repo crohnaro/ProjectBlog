ae19d0722026bad867848fec66e2d9c348a956a2
crohnaro <crohnarodev@gmail.com>
2023-04-22 23:10:36 -0300

    feat: :sparkles: creating the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,82 @@
-import { Inter } from 'next/font/google'
+import { useTheme } from "@mui/material/styles";
+import TemplateDefault from "../src/templates/Default";
+import { Container, Grid, Typography } from "@mui/material";
 
-
-const inter = Inter({ subsets: ['latin'] })
+import MainPost from "app/src/components/MainPost/MainPost";
+import FeaturedPost from "app/src/components/FeaturedPost/FeaturedPost";
 
 export default function Home() {
+  const theme = useTheme();
   return (
     <>
-      <h1>Teste</h1>
+      <TemplateDefault>
+        <Container>
+          <Typography component="h1" variant="h3" color="secondary" align="center" padding="5%">
+            Feed
+          </Typography>
+        </Container>
+        <Container maxWidth="lg">
+          <MainPost 
+          image="https://picsum.photos/200/300"
+          imageText="Tester"
+          title="Test Title"
+          description="Test Description"
+          linkText="Entre aqui"
+          />
+        </Container>
+        <Container maxWidth="lg">
+          <Grid container spacing={4}>
+            <Grid item xs={12} sm={6}>
+              <FeaturedPost 
+                data="10/05/2022"
+                description="Descrição"
+                title="Titulo"
+                image="https://picsum.photos/200/300"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}  >
+              <FeaturedPost 
+                data="10/05/2022"
+                description="Descrição"
+                title="Titulo"
+                image="https://picsum.photos/200/300"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6} >
+              <FeaturedPost 
+                data="10/05/2022"
+                description="Descrição"
+                title="Titulo"
+                image="https://picsum.photos/200/300"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FeaturedPost 
+                data="10/05/2022"
+                description="Descrição"
+                title="Titulo"
+                image="https://picsum.photos/200/300"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FeaturedPost 
+                data="10/05/2022"
+                description="Descrição"
+                title="Titulo"
+                image="https://picsum.photos/200/300"
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FeaturedPost 
+                data="10/05/2022"
+                description="Descrição"
+                title="Titulo"
+                image="https://picsum.photos/200/300"
+              />
+            </Grid>
+          </Grid>
+        </Container>
+      </TemplateDefault>
     </>
-  )
+  );
 }
