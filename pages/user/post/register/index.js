f47f637d62da4ca72d289b60787ad091527b5f71
crohnaro <crohnarodev@gmail.com>
2023-04-28 10:46:39 -0300

    style: :lipstick: updating textfields and creating the button

--- pages/user/post/register/index.js
+++ pages/user/post/register/index.js
@@ -1,23 +1,67 @@
-import { Box, Container, FormControl, Input, InputLabel, FormHelperText } from '@mui/material';
+import { Box, Container, FormControl, FormHelperText, TextField, Typography, Button } from '@mui/material';
 import TemplateDefault from '../../../../src/templates/Default'
+import { styled } from '@mui/material/styles';
+
+import styles from '../../../../styles/Register.module.css'
 
 const UserPostRegister = () => {
+
+  const CssTextField = styled(TextField)({
+    '& label': {
+      color: 'white',
+    },
+    '& label.Mui-focused': {
+      color: 'white',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: 'white',
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: 'white',
+      },
+      '&:hover fieldset': {
+        borderColor: 'white',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: 'white',
+      },
+    },
+  });
+
+  const ColorButton = styled(Button)({
+    color: 'white',
+  });
   return (
     <>
       <TemplateDefault>
         <form>
-          <Container>
+          <Container maxWidth ="md" className={styles.container}>
+            <Box>
+              <Typography component='h1' variant='h3' align='center'color='secondary' margin="100px 0 100px 0">Criação do Anúncio</Typography>
+            </Box>
             <Box>
               <FormControl fullWidth>
-                <InputLabel>Titulo do Anúncio</InputLabel>
-                <Input
+                <CssTextField
                   name="title"
-                  value=""
+                  label="Titulo do Anúncio"
+                  id='custom-css-outlined-input'
+                />
+                <FormHelperText>
+                  error
+                </FormHelperText>
+                <CssTextField
+                  name="title"
+                  label="Descrição"
+                  multiline
+                  rows={6}
+                  id='custom-css-outlined-input'
                 />
                 <FormHelperText>
                   error
                 </FormHelperText>
               </FormControl>
+              <ColorButton size='large' variant='contained'>CRIAR ANÚNCIO</ColorButton>
             </Box>
           </Container>
         </form>
